79a048f58fe9dc07c74ed57decdc71211980bad7
rodion228235 <rodionkovalenko201@gmail.com>
2025-02-25 15:02:31 +0200

    Update forms.py

--- data/forms.py
+++ data/forms.py
@@ -2,12 +2,15 @@ from flask_wtf import FlaskForm
 import wtforms
 
 
-class SignUP(FlaskForm):
-    first_name = wtforms.StringField("Введіть ім'я")
-    last_name = wtforms.StringField("Введіть призвіще")
-    email = wtforms.EmailField("Email", validators=[wtforms.validators.Email()])
-    password = wtforms.PasswordField("Введіть пароль", validators=[wtforms.validators.length(8)])
-    
-    
+class SignUpForm(FlaskForm):
+    first_name = wtforms.StringField("Bведiть iм'я")
+    last_name = wtforms.StringField("Bвeдiть iм'я")
+    email = wtforms.EmailField("Email", validators=[wtforms.validators.DataRequired(), wtforms.validators.Email()])
+    password = wtforms.PasswordField("Пapoль", validators=[wtforms.validators.length(8)]) 
+    submit = wtforms.SubmitField("Зареєструватиcь")
+
+
 class LoginForm(FlaskForm):
-    email = wtforms.EmailField("Email", validators=[wtforms.validators.DataRequired(), wtforms.validators 
+    email = wtforms.EmailField("Email", validators=[wtforms.validators.DataRequired(), wtforms.validators.Email()]) 
+    password = wtforms.PasswordField("Пapoль", validators=[wtforms.validators.length(8)]) 
+    submit = wtforms.SubmitField("Увiйти")
